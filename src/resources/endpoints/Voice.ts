366c1ad5d047300d220016f78b42442498835f05
K. Akatsuki <thevortexspirit@gmail.com>
2021-09-10 00:38:28 -0400

    doc(Voice): update `ListVoiceRegion` desc

--- src/resources/endpoints/Voice.ts
+++ src/resources/endpoints/Voice.ts
@@ -1,8 +1,8 @@
 import type { VoiceRegion } from '../../';
 
 /**
- * Returns an array of voice region objects that can be used when creating
- * servers.
+ * Returns an array of voice region objects that can be used when setting a
+ * voice or stage channel's `rtc_region`.
  *
  * @endpoint [GET](https://discord.com/developers/docs/resources/voice#list-voice-regions) `/voice/regions`
  */
